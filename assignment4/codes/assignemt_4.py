6ae6449cf63568093a916917646e499caaf4e7fe
shreyaswankhede12 <102684318+shreyaswankhede12@users.noreply.github.com>
2022-05-12 22:11:40 +0530

    Add files via upload

---
+++ assignment4/codes/assignemt_4.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+x=np.arange(1,53,1)#all cards in deck is given linear nos
+a=np.random.randint(1,53,size=1)#generated a random card
+b=np.random.randint(1,53,size=1)#generated another random card
+arr=np.arange(1,53,13)#array of positions with aces marked
+l=len(x)
+k=len(arr)
+if(a in arr and b in arr ):
+    print("The practical probability is :",(k/l)*(k/l))
+if((a in arr and b not in arr) or (a not in arr and b in arr)):
+    print("The practical probability is :",(k/l)*((l-k)/l))
+if(a not in arr and b not in arr):
+    print("The practical probability is :",((l-k)/l)*((l-k)/l))
+
+#theorotical probability
+print("therotical probabilty for 2 aces is: ",(4/52)*(4/52))
+print("therotical probabilty for 1 ace and 1 non ace is: ",(4/52)*(48/52))
+print("therotical probabilty for 2 non aces is: ",(48/52)*(48/52))
